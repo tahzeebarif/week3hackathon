8d9c3fc8104762a114592ad3029d066146bc5f0a
tahzeebarif <tahzeebarif3@gmail.com>
2026-03-16 10:06:13 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
